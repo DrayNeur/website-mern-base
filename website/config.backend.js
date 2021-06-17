66fd161bf0701ddad96aa73c9c733416d964b5e3
DrayNeur <50057580+DrayNeur@users.noreply.github.com>
2021-06-17 18:42:57 +0200

    fixed build backend

--- website/config.backend.js
+++ website/config.backend.js
@@ -1,5 +1,15 @@
-const path = require('path');
+var webpack = require('webpack');
+var path = require('path');
+var fs = require('fs');
 
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function (x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function (mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
 module.exports = {
   entry: './website.js',
   mode: 'production',
@@ -8,4 +18,10 @@ module.exports = {
     path: path.resolve(__dirname, 'build'),
     filename: 'main.js',
   },
+  externals: nodeModules,
+  plugins: [
+    new webpack.IgnorePlugin(/\.(css|less)$/),
+    new webpack.BannerPlugin('require("source-map-support").install();',
+                             { raw: true, entryOnly: false })
+  ]
 };

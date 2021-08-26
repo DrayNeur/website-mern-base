a614c34d7f3161177eb5ed441592c5e822f01753
Dimitri Ivanov <50057580+DrayNeur@users.noreply.github.com>
2021-08-26 11:43:43 +0200

    Update config.build.js

--- website/config.build.js
+++ website/config.build.js
@@ -6,6 +6,12 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
